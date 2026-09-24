53e644c50d56d38160d402ff7089f438b912e28b
Sachin Kumar <sr82979@gmail.com>
2026-09-24 18:46:04 +0530

    Update programs 2 and 3 with latest code

--- src/program_2UserInput.java
+++ src/program_2UserInput.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class program2UserInput {
+public class program_2UserInput {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
---
+++ src/program_3AddTwoNumbers.java
@@ -0,0 +1,2 @@
+public class program_3AddTwoNumbers {
+}
