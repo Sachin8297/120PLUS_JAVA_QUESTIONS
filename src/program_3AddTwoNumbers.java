2cb44b73dfbbc849537940105f82de18371deca1
Sachin Kumar <sr82979@gmail.com>
2026-09-24 21:57:17 +0530

    Update programs 2 and 3 with latest code

--- src/program_3AddTwoNumbers.java
+++ src/program_3AddTwoNumbers.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class program_3AddTwoNumbers {
     public static void main(String args[]){
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);// jo bhi system pr input diya h usko utha lo
         System.out.print("Enter the two number: ");
-        int num1 = input.nextInt();
+        int num1 = input.nextInt();// jo bhi system se phla input aya h usko isme dal do
         int num2 = input.nextInt();
         int sum = num1+num2;
         System.out.print("The sum of two numbers is : "+ sum);
