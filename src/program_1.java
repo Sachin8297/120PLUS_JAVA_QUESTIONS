1fbc01de58052e4408bc9a0473488aa20f46f50b
Sachin Kumar <sr82979@gmail.com>
2026-09-24 18:01:34 +0530

    Add new Java question

--- src/program_1.java
+++ src/program_1.java
@@ -1,2 +1,14 @@
+/* Show the right half pyramid using the single print statement .*/
 public class program_1 {
+    public static void main(String args[]){
+        System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.print("\n\n");
+        System.out.print("* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.print("\n\n");
+        System.out.println("         *\n      * *\n    * * *\n  * * * *\n* * * * *");
+
+
+    }
 }
+
+/* Show the right half pyramid using the single print statement .*/
