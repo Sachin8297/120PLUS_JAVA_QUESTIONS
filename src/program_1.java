dedf279539ff8b2b474417da118560398bd03a81
Sachin Kumar <sr82979@gmail.com>
2026-09-24 18:39:03 +0530

    Add new Java question

--- src/program_1.java
+++ src/program_1.java
@@ -3,7 +3,7 @@ public class program_1 {
     public static void main(String args[]){
         System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
         System.out.print("\n\n");
-        System.out.print("* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.print("* * * * *\n* * * *\n* * *\n* *\n*"); // \n is uesd to print the statement in the same line
         System.out.print("\n\n");
         System.out.println("         *\n      * *\n    * * *\n  * * * *\n* * * * *");
 
